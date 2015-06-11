95e1687c2b393d66f30d758d389017236e1a84c7
mugwort_rc <mugwort.rc@gmail.com>
2015-06-11 16:06:33 +0900

    Add collect border.

--- src/utils/excel.py
+++ src/utils/excel.py
@@ -15,24 +15,35 @@ POINTS_TYPE = [
 ]
 
 class Style(object):
-    def __init__(self, pattern, fore, back):
+    def __init__(self, pattern, fore, back, style, color, weight):
+        # format.fill
         self.pattern = pattern
         self.fore = fore
         self.back = back
+        # border
+        self.style = style
+        self.color = color
+        self.weight = weight
 
     def isFilled(self):
         return self.pattern <= 0 or self.pattern > 48
 
     @staticmethod
     def create():
-        return Style(-1, lergb2rgb(0), lergb2rgb(0xffffff))
+        # Format.Fill: None, black, white
+        return Style(-2, lergb2rgb(0), lergb2rgb(0xffffff),
+        # Border: Auto, None, None
+                      -4105, -2, -4142)
 
     @staticmethod
     def from_com_object(obj):
         return Style(
-            obj.Pattern,
-            lergb2rgb(obj.ForeColor.RGB),
-            lergb2rgb(obj.BackColor.RGB)
+            obj.Format.Fill.Pattern,
+            lergb2rgb(obj.Format.Fill.ForeColor.RGB),
+            lergb2rgb(obj.Format.Fill.BackColor.RGB),
+            obj.Border.LineStyle,
+            obj.Border.Color,
+            obj.Border.Weight,
         )
 
 
@@ -57,19 +68,19 @@ def rgb2lergb(r, g, b):
 
 def collect_styles(chart, prog=None):
     if chart.ChartType in POINTS_TYPE:
-        return list(_collect_fill(chart.SeriesCollection(1).Points, prog=prog))
+        return list(_collect_style(chart.SeriesCollection(1).Points, prog=prog))
     else:
-        return list(_collect_fill(chart.SeriesCollection, prog=prog))
+        return list(_collect_style(chart.SeriesCollection, prog=prog))
 
 
-def _collect_fill(method, prog=None):
+def _collect_style(method, prog=None):
     maximum = method().Count
     if prog:
         prog.initialize(maximum)
     for i in com_range(maximum):
         if prog:
             prog.update(i)
-        obj = method(i).Format.Fill
+        obj = method(i)
         yield Style.from_com_object(obj)
     if prog:
         prog.finish()
@@ -77,12 +88,12 @@ def _collect_fill(method, prog=None):
 
 def apply_styles(chart, styles, prog=None):
     if chart.ChartType in POINTS_TYPE:
-        _apply_fill(chart.SeriesCollection(1).Points, styles, prog=prog)
+        _apply_style(chart.SeriesCollection(1).Points, styles, prog=prog)
     else:
-        _apply_fill(chart.SeriesCollection, styles, prog=prog)
+        _apply_style(chart.SeriesCollection, styles, prog=prog)
 
 
-def _apply_fill(method, styles, prog=None):
+def _apply_style(method, styles, prog=None):
     maximum = min(method().Count, len(styles))
     if prog:
         prog.initialize(maximum)
@@ -90,13 +101,19 @@ def _apply_fill(method, styles, prog=None):
         if prog:
             prog.update(i)
         style = styles[i-1]  # 0-based
-        obj = method(i).Format.Fill
+        obj = method(i)
+        # Format.Fill
         if style.isFilled():
-            obj.Solid()
-            obj.ForeColor.RGB = rgb2lergb(*style.fore)
+            obj.Format.Fill.Solid()
+            obj.Format.Fill.ForeColor.RGB = rgb2lergb(*style.fore)
         else:
-            obj.Patterned(style.pattern)
-            obj.ForeColor.RGB = rgb2lergb(*style.fore)
-            obj.BackColor.RGB = rgb2lergb(*style.back)
+            obj.Format.Fill.Patterned(style.pattern)
+            obj.Format.Fill.ForeColor.RGB = rgb2lergb(*style.fore)
+            obj.Format.Fill.BackColor.RGB = rgb2lergb(*style.back)
+        # Border
+        obj.Border.LineStyle = style.style
+        if style.style not in [-4142, -4105]:
+            obj.Border.Color = style.color
+            obj.Border.Weight = style.weight
     if prog:
         prog.finish()
