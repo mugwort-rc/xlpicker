f7fd448d3c8833f5c56269161c4be6ec6d9ec911
mugwort_rc <mugwort.rc@gmail.com>
2015-06-11 16:05:46 +0900

    Update debug tool.

--- src/excel.py
+++ src/excel.py
@@ -1,180 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import pythoncom
 
 def excel_range(n):
     return range(1, n+1)
 
 
-class Collector(object):
-    def __init__(self, name):
-        """
-        :type name: str
-        """
-        self.name = name
+COLLECT_METHOD = 1
+COLLECT_ATTRIBUTE = 2
 
-    def collect(self, parent):
-        """
-        :type parent: COMObject
-        :return: collected data
-        """
-        raise NotImplementedError
-
-    @property
-    def name(self):
-        """
-        :rtype: str
-        """
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        """
-        :type name: str
-        """
-        self._name = name
-
-
-class SequenceCollector(Collector):
-    def __init__(self, name, collector):
-        """
-        :type name: str
-        :type collector: Collector
-        """
-        super(SequenceCollector, self).__init__(name)
-        self.collector = collector
-
-    def collect(self, parent):
-        """
-        :type parent: COMObject
-        :return: collected data
-        """
-        return [self.collector.collect(child) for child in self.childrens(parent)]
-
-    def childrens(self, parent):
-        """
-        :type parent: COMObject
-        :rtype: collections.Iterable[COMObject]
-        """
-        raise NotImplementedError
-
-
-class AttributeSequenceCollector(SequenceCollector):
-
-    ATTRIBUTE_NAME = None
-
-    def __init__(self, collector):
-        if self.ATTRIBUTE_NAME is None:
-            raise NotImplementedError
-        super(AttributeSequenceCollector, self).__init__(self.ATTRIBUTE_NAME, collector)
-
-    def childrens(self, parent):
-        attrib = getattr(parent, self.ATTRIBUTE_NAME)
-        for i in excel_range(attrib.Count):
-            yield attrib(i)
-
-class MethodSequenceCollector(AttributeSequenceCollector):
-    def childrens(self, parent):
-        attrib = getattr(parent, self.ATTRIBUTE_NAME)
-        for i in excel_range(attrib().Count):
-            yield attrib(i)
-
-
-class AttributeCollector(Collector):
-
-    ATTRIBUTE_NAME = None
-
-    def __init__(self):
-        if self.ATTRIBUTE_NAME is None:
-            raise NotImplementedError
-        super(AttributeCollector, self).__init__(self.ATTRIBUTE_NAME)
-
-    def collect(self, parent):
-        raise NotImplementedError
-
-class AttributeValueCollector(AttributeCollector):
-    def collect(self, parent):
-        return getattr(parent, self.ATTRIBUTE_NAME)
-
-
-class SetCollector(AttributeCollector):
-    def __init__(self, collectors):
-        super(SetCollector, self).__init__()
-        self.collectors = collectors
-
-class ObjectCollector(SetCollector):
-    def collect(self, parent):
-        # directly
-        return dict([(c.name, c.collect(parent)) for c in self.collectors])
-
-class PropertyCollector(SetCollector):
-    def collect(self, parent):
-        obj = getattr(parent, self.ATTRIBUTE_NAME)
-        return dict([(c.name, c.collect(obj)) for c in self.collectors])
-
-class MethodCollector(SetCollector):
-    def collect(self, parent):
-        obj = getattr(parent, self.ATTRIBUTE_NAME)()
-        return dict([(c.name, c.collect(obj)) for c in self.collectors])
-
-
-# Values
-
-class BrightnessCollector(AttributeValueCollector):
-    ATTRIBUTE_NAME = "Brightness"
-
-class ObjectThemeColorCollector(AttributeValueCollector):
-    ATTRIBUTE_NAME = "ObjectThemeColor"
-
-class PatternCollector(AttributeValueCollector):
-    ATTRIBUTE_NAME = "Pattern"
-
-class RGBCollector(AttributeValueCollector):
-    ATTRIBUTE_NAME = "RGB"
-
-class SchemeColorCollector(AttributeValueCollector):
-    ATTRIBUTE_NAME = "SchemeColor"
-
-class TypeCollector(AttributeValueCollector):
-    ATTRIBUTE_NAME = "Type"
-
-
-class ColorFormatCollector(PropertyCollector):
-    pass
-
-class BackColorCollector(ColorFormatCollector):
-    ATTRIBUTE_NAME = "BackColor"
-
-class ForeColorCollector(ColorFormatCollector):
-    ATTRIBUTE_NAME = "ForeColor"
-
-
-class FillFormatCollector(PropertyCollector):
-    pass
-
-class FillCollector(FillFormatCollector):
-    ATTRIBUTE_NAME = "Fill"
-
-class ChartFormatCollector(PropertyCollector):
-    pass
-
-class FormatCollector(ChartFormatCollector):
-    ATTRIBUTE_NAME = "Format"
-
-
-# Charts
-
-class SeriesCollector(ObjectCollector):
-    ATTRIBUTE_NAME = "Series"  # DUMMY
-
-class SeriesCollectionCollector(MethodSequenceCollector):
-    ATTRIBUTE_NAME = "SeriesCollection"
-
-
-# books
-
-class WorkbooksCollector(AttributeSequenceCollector):
-    ATTRIBUTE_NAME = "Workbooks"
-
-class WorksheetsCollector(AttributeSequenceCollector):
-    ATTRIBUTE_NAME = "Worksheets"
-
+def collect(obj, rule):
+    result = {}
+    try:
+        for key, value in rule.items():
+            if value is None or callable(value):
+                result[key] = getattr(obj, key)
+                if callable(value):
+                    result[key] = value(result[key])
+            else:
+                next_obj = getattr(obj, key)
+                if isinstance(value, dict):
+                    result[key] = collect(next_obj, value)
+                else:
+                    type, next_rule = value
+                    count = 0
+                    if type == COLLECT_METHOD:
+                        count = next_obj().Count
+                    elif type == COLLECT_ATTRIBUTE:
+                        count = next_obj.Count
+                    result[key] = [collect(next_obj(x), next_rule) for x in excel_range(count)]
+    except (AttributeError, pythoncom.com_error):
+        print(rule)
+        raise
+    return result
--- src/tools/dump.py
+++ src/tools/dump.py
@@ -23,23 +23,34 @@ def main(argv):
     if XL.ActiveChart is None:
         print("ActiveChart is None", file=sys.stderr)
         return 1
-    colorCollectors = [
-        excel.RGBCollector(),
-        excel.SchemeColorCollector(),
-    ]
 
-    collector = excel.SeriesCollectionCollector(
-        excel.SeriesCollector([
-            excel.FormatCollector([
-                excel.FillCollector([
-                    excel.BackColorCollector(colorCollectors),
-                    excel.ForeColorCollector(colorCollectors),
-                    excel.PatternCollector(),
-                ])
-            ])
-        ])
-    )
-    print(yaml.safe_dump(collector.collect(XL.ActiveChart)))
+    colorLambda = lambda x: "#{:02x}{:02x}{:02x}".format(x&0xff, (x>>8)&0xff, (x>>16)&0xff) if x >= 0 else x
+    colorCollector = {
+        "RGB": colorLambda,
+    }
+    formatCollector = {
+        "Fill": {
+            "BackColor": colorCollector,
+            "ForeColor": colorCollector,
+            "Type": None,
+            "Visible": None,
+        },
+    }
+    collector = {
+        "SeriesCollection": (excel.COLLECT_METHOD, {
+            "Format": formatCollector,
+            "Points": (excel.COLLECT_METHOD, {
+                "Format": formatCollector,
+            }),
+            "Border": {
+                "Color": colorLambda,
+                "ColorIndex": None,
+                "LineStyle": None,
+                "Weight": None,
+            },
+        }),
+    }
+    print(yaml.safe_dump(excel.collect(XL.ActiveChart, collector)))
     return 0
 
 
