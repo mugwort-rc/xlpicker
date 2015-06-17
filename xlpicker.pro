748c610bbc034bdb83452bbd037b8edc33ebabed
mugwort_rc <mugwort.rc@gmail.com>
2015-06-17 17:18:23 +0900

    Add translation.

---
+++ xlpicker.pro
@@ -0,0 +1,13 @@
+SOURCES = \
+  src/main.py \
+  src/mainwindow.py \
+  src/models/abstract.py \
+  src/models/excel/style.py \
+  src/utils/progress.py
+
+FORMS = src/ui/mainwindow.ui
+
+TRANSLATIONS = xlpicker_ja.ts
+
+CODECFORTR = UTF-8
+CODECFORSRC = UTF-8
---
+++ xlpicker_ja.ts
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>ChartPatternDelegate</name>
+    <message>
+        <location filename="src/models/excel/style.py" line="290"/>
+        <source>Auto</source>
+        <translation>自動</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="291"/>
+        <source>Solid</source>
+        <translation>実線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="292"/>
+        <source>SquareDot</source>
+        <translation>点線 (角)</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="293"/>
+        <source>RoundDot</source>
+        <translation>点線 (丸)</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="294"/>
+        <source>LineDash</source>
+        <translation>破線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="295"/>
+        <source>DashDot</source>
+        <translation>一点鎖線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="296"/>
+        <source>DashDotDot</source>
+        <translation>二点鎖線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="297"/>
+        <source>LongDash</source>
+        <translation>長破線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="298"/>
+        <source>LongDashDot</source>
+        <translation>長鎖線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="299"/>
+        <source>None</source>
+        <translation>なし</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="302"/>
+        <source>Hairline</source>
+        <translation>最細</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="303"/>
+        <source>Thin</source>
+        <translation>細</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="304"/>
+        <source>Medium</source>
+        <translation>普通</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="305"/>
+        <source>Thick</source>
+        <translation>太線</translation>
+    </message>
+</context>
+<context>
+    <name>ChartStyleModel</name>
+    <message>
+        <location filename="src/models/excel/style.py" line="101"/>
+        <source>Unknown</source>
+        <translation>不明</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="103"/>
+        <source>Auto</source>
+        <translation>自動</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="104"/>
+        <source>Solid</source>
+        <translation>実線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="105"/>
+        <source>SquareDot</source>
+        <translation>点線 (角)</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="106"/>
+        <source>RoundDot</source>
+        <translation>点線 (丸)</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="107"/>
+        <source>LineDash</source>
+        <translation>破線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="108"/>
+        <source>DashDot</source>
+        <translation>一点鎖線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="109"/>
+        <source>DashDotDot</source>
+        <translation>二点鎖線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="110"/>
+        <source>LongDash</source>
+        <translation>長破線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="111"/>
+        <source>LongDashDot</source>
+        <translation>長鎖線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="112"/>
+        <source>None</source>
+        <translation>なし</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="116"/>
+        <source>Hairline</source>
+        <translation>最細</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="117"/>
+        <source>Thin</source>
+        <translation>細</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="118"/>
+        <source>Medium</source>
+        <translation>普通</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="119"/>
+        <source>Thick</source>
+        <translation>太線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="172"/>
+        <source>Pattern</source>
+        <translation>パターン</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="174"/>
+        <source>Fore</source>
+        <translation>前景色</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="176"/>
+        <source>Back</source>
+        <translation>背景色</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="178"/>
+        <source>Border</source>
+        <translation>線</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="180"/>
+        <source>Border color</source>
+        <translation>線色</translation>
+    </message>
+    <message>
+        <location filename="src/models/excel/style.py" line="182"/>
+        <source>Border weight</source>
+        <translation>太さ</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="29"/>
+        <source>1. Pickup style</source>
+        <translation>1. スタイルを選択</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="35"/>
+        <source>from ActiveChart</source>
+        <translation>現在のグラフから</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="45"/>
+        <source>2. Select target to</source>
+        <translation>2. 対象を選択</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="55"/>
+        <source>ActiveChart</source>
+        <translation>現在のグラフ</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="81"/>
+        <source>Apply</source>
+        <translation>適用</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="42"/>
+        <source>ActiveBook</source>
+        <translation>現在のブック</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="109"/>
+        <source>&amp;Styles</source>
+        <translation>スタイル (&amp;S)</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="121"/>
+        <source>New</source>
+        <translation>新規</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="126"/>
+        <source>Up</source>
+        <translation>上げる</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="131"/>
+        <source>Down</source>
+        <translation>下げる</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="136"/>
+        <source>Remove</source>
+        <translation>削除</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="42"/>
+        <source>ActiveSheet</source>
+        <translation>現在のシート</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="46"/>
+        <source>All Chart</source>
+        <translation>全てのグラフ</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="46"/>
+        <source>Type of ActiveChart</source>
+        <translation>現在のグラフタイプ</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="74"/>
+        <source>Error</source>
+        <translation>エラー</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="74"/>
+        <source>Chart is not active currently.</source>
+        <translation>グラフが選択されていません。</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="114"/>
+        <source>Collect error</source>
+        <translation>収集エラー</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="114"/>
+        <source>Failed collecting from the ActiveChart.</source>
+        <translation>現在のグラフからのスタイル収集に失敗しました。</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="174"/>
+        <source>Apply error</source>
+        <translation>適用エラー</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="170"/>
+        <source>Failed applied in the ActiveChart.</source>
+        <translation>現在のグラフに適用できませんでした。</translation>
+    </message>
+    <message>
+        <location filename="src/mainwindow.py" line="174"/>
+        <source>Failed applied.</source>
+        <translation>適用に失敗しました。</translation>
+    </message>
+    <message>
+        <location filename="src/ui/mainwindow.ui" line="14"/>
+        <source>xlpicker</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
