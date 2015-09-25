76f4a74fb27286837814cd031725a47b257d472f
mugwort_rc <mugwort.rc@gmail.com>
2015-09-25 17:26:55 +0900

    bugfix Border error.

--- src/utils/excel.py
+++ src/utils/excel.py
@@ -116,9 +116,9 @@ def _apply_style(method, styles, prog=None):
             obj.Format.Fill.BackColor.RGB = rgb2lergb(*style.back)
         # Border
         obj.Border.LineStyle = style.style
-        obj.Format.Line.DashStyle = style.dash
-        obj.Format.Line.Style = style.line
         if style.style not in [-4142, -4105]:  # None, Auto
+            obj.Format.Line.DashStyle = style.dash
+            obj.Format.Line.Style = style.line
             obj.Border.Color = rgb2lergb(*style.color)
             obj.Border.Weight = style.weight
     if prog:
